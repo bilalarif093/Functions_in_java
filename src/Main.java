aedf79225e7d0e5651831e90525fd791c3004849
BilalArif <bilalarif093@gmail.com>
2024-08-05 09:33:00 +0500

    second

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
+        print();
+    }
+    public static void print(){
+        System.out.println("message1");
+        print2();
+    }
+
+    public static void print2() {
+        System.out.println("message2");
     }
 }
